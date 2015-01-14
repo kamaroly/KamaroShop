3455a508c33c7f15a551a09249cfca662d817297
Kamaro Lambert <kamaroly@gmail.com>
2015-01-14 15:17:39 +0200

    Added way generator

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'kamaro',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'default_',
